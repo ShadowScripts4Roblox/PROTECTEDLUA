5f4b06155984f8acb1794602ecc52f9ae8d6780b
ShadowScripts4Roblox <126200271+ShadowScripts4Roblox@users.noreply.github.com>
2023-03-07 20:28:40 +0200

    Create Loader52.lua

---
+++ Scripts/DOORS/Loader52.lua
@@ -0,0 +1,14 @@
+local s = Instance.new("Sound")
+s.Name = "Sound"
+s.SoundId = "http://www.roblox.com/asset/?id=4520789398"
+s.Volume = 100
+s.Looped = false
+s.archivable = false
+
+s.Parent = game.Workspace
+
+wait(0.1)
+s:play()
+wait(0.2)
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/ShadowScripts4Roblox/PROTECTEDLUA/main/Scripts/DOORS/5.2.lua'))()
