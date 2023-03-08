e1d9068064cd5cf173225965cc10a9119b2c8caa
ShadowScripts4Roblox <126200271+ShadowScripts4Roblox@users.noreply.github.com>
2023-03-08 16:40:59 +0200

    Update Loader52.lua

--- Scripts/DOORS/Loader52.lua
+++ Scripts/DOORS/Loader52.lua
@@ -12,56 +12,56 @@ ss:play()
 
 
 print("0%")
-wait(1)
+wait(3)
 
 game.StarterGui:SetCore("SendNotification", {
 Title = "Shadow Scripts - DOORS";
 Text = "0%";
 Icon = "rbxassetid://6646175695"; -- the image if u want. 
-Duration = 1; -- how long the notification should in secounds
+Duration = 2.8; -- how long the notification should in secounds
 })
 
-wait(1)
+wait(3)
 print("25%")
 
 game.StarterGui:SetCore("SendNotification", {
 Title = "Shadow Scripts - DOORS";
 Text = "25%";
 Icon = "rbxassetid://6646175695"; -- the image if u want. 
-Duration = 1; -- how long the notification should in secounds
+Duration = 2.8; -- how long the notification should in secounds
 })
 
-wait(1)
+wait(3)
 print("50%")
 
 game.StarterGui:SetCore("SendNotification", {
 Title = "Shadow Scripts - DOORS";
 Text = "50%";
 Icon = "rbxassetid://6646175695"; -- the image if u want. 
-Duration = 1; -- how long the notification should in secounds
+Duration = 2.8; -- how long the notification should in secounds
 })
 
-wait(1)
+wait(3)
 print("75%")
 
 game.StarterGui:SetCore("SendNotification", {
 Title = "Shadow Scripts - DOORS";
 Text = "75%";
 Icon = "rbxassetid://6646175695"; -- the image if u want. 
-Duration = 1; -- how long the notification should in secounds
+Duration = 2.8; -- how long the notification should in secounds
 })
 
-wait(1)
+wait(3)
 
 game.StarterGui:SetCore("SendNotification", {
 Title = "Shadow Scripts - DOORS";
 Text = "100% (Script Loaded)";
 Icon = "rbxassetid://6646175695"; -- the image if u want. 
-Duration = 1; -- how long the notification should in secounds
+Duration = 2.8; -- how long the notification should in secounds
 })
 
 print("100%")
-wait(1)
+wait(3)
 
 
 
@@ -74,7 +74,8 @@ s.archivable = false
 
 s.Parent = game.Workspace
 
+loadstring(game:HttpGet('https://raw.githubusercontent.com/ShadowScripts4Roblox/PROTECTEDLUA/main/Scripts/DOORS/5.2.lua'))()
 wait(0.1)
 s:play()
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/ShadowScripts4Roblox/PROTECTEDLUA/main/Scripts/DOORS/5.2.lua'))()
+
